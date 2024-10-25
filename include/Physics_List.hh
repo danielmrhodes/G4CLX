0f826718f5719b38cf2a15785a524d9da04403d5
rhodes25 <rhodes25@bugatti.llnl.gov>
2024-10-25 13:56:09 -0700

    Switch to standard EM_opt4 physics list

--- include/Physics_List.hh
+++ include/Physics_List.hh
@@ -1,9 +1,13 @@
 #ifndef Physics_List_h
 #define Physics_List_h 1
 
-#include "G4VUserPhysicsList.hh"
+//#include "G4VUserPhysicsList.hh"
+//class Physics_List : public G4VUserPhysicsList {
 
-class Physics_List : public G4VUserPhysicsList {
+
+#include "G4VModularPhysicsList.hh"
+
+class Physics_List : public G4VModularPhysicsList {
 
 public:
    
