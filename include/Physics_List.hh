206f4a18ccc936308d8ba45b9f8919913ef2bf2b
rhodes25 <rhodes25@bugatti.llnl.gov>
2025-04-18 11:18:12 -0700

    Changing how the Physics List works. Disabled multiple scattering for Generic Ions. Removed commented-out code.

--- include/Physics_List.hh
+++ include/Physics_List.hh
@@ -15,40 +15,4 @@ public:
 
 };
 
-
-/*
-#include "G4VModularPhysicsList.hh"
-#include "G4VPhysicsConstructor.hh"
-
-class Physics_List_Constructor : public G4VPhysicsConstructor {
-
-public:
-
-  explicit Physics_List_Constructor(G4int ver=1, const G4String& name="");
-  ~Physics_List_Constructor() override;
-  
-  void ConstructParticle() override;
-  void ConstructProcess() override;
-
-private:
-  G4int verbose;
-
-};
-
-class Physics_List : public G4VModularPhysicsList {
-
-public:
-
-  Physics_List();
-  ~Physics_List();
-  
-  void ConstructParticle();
-  void ConstructProcess();
-
-private:
-  G4VPhysicsConstructor* fEmPhysicsList;
-
-};
-*/
-
 #endif
